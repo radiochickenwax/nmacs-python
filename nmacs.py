82c21cf35f2f102d872d1d3e95652786bd8f4ede
greg mobile n900t <gregorio.jp.mitchell@gmail.com>
2014-12-24 17:34:39 -0700

    basic line structure established.  need to add key commands.

--- nmacs.py
+++ nmacs.py
@@ -15,13 +15,26 @@ def getStartLine():
 def getFinishLine():
     ""
 def displayLines(lines,stdscr,startLine,finishLine):
-    ""
+    stdscr.move(0,0)
+    for i in range(startLine,finishLine):
+        stdscr.addstr(i,0,lines[i])
+#         cy,cx=stdscr.getyx()
+#         stdscr.move(cy+1,0)
+    stdscr.move(0,0)
 
 def main():
     stdscr = initCurses()
+    lines = [ ]
     #stdscr.printw("hello world")  # no printw in python curses
-    stdscr.addstr("hello world\n")
+    for i in range(0,99):
+        #stdscr.addstr(i + "lots of lines")
+        lines.append(str(i) + "lots of lines")
+    ymax,xmax = stdscr.getmaxyx()
+    displayLines(lines,stdscr,0,ymax-1)
+    #stdscr.move(10,10)
     stdscr.refresh()
+
+
 
     # start repl here
     keypress = stdscr.getch()
